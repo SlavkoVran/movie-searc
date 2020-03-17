be7e6a0fead8017fabdf7b8b3da9ccdac666c8fa
SlavkoVran <slavkov.mob@gmail.com>
2020-03-17 14:57:16 +0100

    Updates

---
+++ precache-manifest.ec285e3cf4ed6f7c3bd09d97f6d7569f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efaf9447ea7dc406eb1d5e937ca429f",
+    "url": "/movie-searc/index.html"
+  },
+  {
+    "revision": "f8306d0248a781bb7c05",
+    "url": "/movie-searc/static/css/main.ca8d729c.chunk.css"
+  },
+  {
+    "revision": "b5ab879386dcdddab146",
+    "url": "/movie-searc/static/js/2.d8bb3851.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/movie-searc/static/js/2.d8bb3851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8306d0248a781bb7c05",
+    "url": "/movie-searc/static/js/main.dcdacd0e.chunk.js"
+  },
+  {
+    "revision": "2f4cae404554705ad787",
+    "url": "/movie-searc/static/js/runtime-main.47edbc66.js"
+  },
+  {
+    "revision": "7ac3ff313fff6fb94cf44a68835a7ffd",
+    "url": "/movie-searc/static/media/831.7ac3ff31.gif"
+  }
+]);
